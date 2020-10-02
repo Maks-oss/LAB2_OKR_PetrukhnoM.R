f4566eb2314318da744e76b0cf9e2c4a76d5a63c
maksimus <maksimilianos>
2020-10-02 03:15:58 +0300

    UPDATE

--- process.js
+++ process.js
@@ -1,7 +1,7 @@
 
 const parent=document.getElementById('Notes')
 const text=document.getElementById('text')
-
+parent.removeChild(parent.firstChild)
 function getTime() {
     let currentdate = new Date();
     return currentdate.getDate() + "/"
@@ -24,7 +24,6 @@ function add() {
     array.push(child.outerHTML)
 
     parent.appendChild(child)
-
     let arr=Array.from(parent.childNodes)
 
     for (let i = 0; i <arr.length; i++) {
@@ -33,7 +32,7 @@ function add() {
 
             let current = document.getElementsByClassName("active");
 
-            if(current.length===1)if(current[0].className!==undefined)current[0].className = current[0].className.replace(" active", "");
+            if(current.length===1)current[0].className = current[0].className.replace(" active", "");
 
             this.className += " active";
 
@@ -43,7 +42,7 @@ function add() {
             if(arr[i].className==="custom active")
             {
                 text.value=val;
-                textarray[i-1]=val
+                textarray[i]=val
                 localStorage.setItem('N',JSON.stringify(textarray))
             }
 
@@ -55,21 +54,23 @@ function add() {
 window.onload=function () {
     if(array!==null)
     {
-
+        console.log(array.length)
         for(let i=0;i<array.length;i++)
         {
             parent.insertAdjacentHTML('beforeend',array[i])
+            parent.childNodes.item(i).id="note"+(i+1)
             if(textarray!==null)
             {
-                parent.childNodes.item(i+1).innerHTML=textarray[i]+"<br>"+getTime()
+                parent.childNodes.item(i).innerHTML=textarray[i]+"<br>"+getTime()
             }
-            parent.childNodes.item(i+1).addEventListener('click',function () {
+            parent.childNodes.item(i).addEventListener('click',function () {
+                console.log(parent.childNodes.item(i).id)
                 let val=parent.children.item(i).innerHTML.substring(0,parent.children.item(i).innerHTML.indexOf("<"))
                 const current = document.getElementsByClassName("active");
-               if(current.length===1) current[0].className = current[0].className.replace(" active", "");
+                if(current.length===1) current[0].className = current[0].className.replace(" active", "");
                 this.className += " active";
 
-                if(parent.childNodes.item(i+1).className==="custom active") {
+                if(parent.childNodes.item(i).className==="custom active") {
                     textarray[i]=val
                     text.value = textarray[i]
                     localStorage.setItem('N',JSON.stringify(textarray))
@@ -95,28 +96,34 @@ text.addEventListener('input',function () {
 function deleteItem() {
     //localStorage.clear()
     let arr=Array.from(parent.childNodes)
+
     for(let i=0;i<arr.length;i++)
     {
         if(arr[i].className==="custom active"){
             array.splice(-i,1)
-            if(textarray!==null)textarray.splice(i-1,1)
+            if(textarray!==null)textarray.splice(i,1)
             parent.removeChild(arr[i])
         }
     }
-    parent.lastChild.className="custom active"
+    arr=Array.from(parent.childNodes)
+
+    for (let i = 0; i < arr.length; i++) {
+        arr[i].id="note"+(i+1)
+    }
+
     localStorage.removeItem('Note')
     localStorage.removeItem('N')
     localStorage.setItem('N',JSON.stringify(textarray))
     localStorage.setItem('Note',JSON.stringify(array))
-
+    parent.lastChild.className="custom active"
 }
 window.onhashchange=function () {
     let arr=Array.from(parent.childNodes)
-    for (let i = arr.length-1; i >= 0; i--) {
+    for (let i = 0; i < arr.length; i++) {
         if(location.hash==="#"+arr[i].id)
         {
             let a=document.getElementsByClassName("custom active")
-            a[0].className="custom"
+            if(a.length===1)a[0].className="custom"
             arr[i].className="custom active"
             text.value=arr[i].innerHTML.substring(0, arr[i].innerHTML.indexOf("<"))
             break
